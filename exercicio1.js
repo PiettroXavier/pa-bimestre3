c12aabb8acf5b2ac229d123a014a5e238ef0c7c4
PiettroXavier <piettroxavier2016@gmail.com>
2024-08-01 12:32:13 -0300

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,8 @@
+let v1 = "9797"
+let v2 = "2"
+
+if (v1 > v2) {
+    console.log("O Maior Valor é:", v1)
+} else {
+    console.log("O Maior Valor é:", v2)
+}
---
+++ exercicio3.js
@@ -0,0 +1,9 @@
+let n1 = 5
+let n2 = 7.5
+let m = (n1+n2)/2
+
+if ( m >= 7 ) {
+    console.log("Você está aprovado(a) com a média:", m)
+} else {
+    console.log("Você está reprovado(a) com a media", m,". Você precisa estudar mais!")
+}
